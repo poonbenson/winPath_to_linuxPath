a566569ba30df0cc6b1359c8c20e33412f97ff02
poonbenson <poon.benson@gmail.com>
2024-02-27 10:31:53 +0000

    add a dummy comment

--- winPath_to_linuxPath.py
+++ winPath_to_linuxPath.py
@@ -14,6 +14,9 @@ def main():
     clipboardTXT = pyperclip.paste()
     clipboardTXT = clipboardTXT.replace('\\', '/')
     pyperclip.copy(clipboardTXT)
+    '''
+    done
+    '''
 
 
 if __name__ == '__main__':
